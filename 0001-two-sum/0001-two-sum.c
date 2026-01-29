09a826ae4a71c2ae53093292571e6a54bfcf67de
Dhaksita R <dhaksitarajendran@gmail.com>
2026-01-29 12:19:55 +0530

    Update 0001-two-sum.c

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,15 +1,12 @@
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[j] == target - nums[i]) {
-                int* result = malloc(sizeof(int) * 2);
-                result[0] = i;
-                result[1] = j;
-                *returnSize = 2;
-                return result;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
+        return new int[]{};
     }
-    *returnSize = 0;
-    return malloc(sizeof(int) * 0);
 }
